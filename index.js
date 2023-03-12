43a8569f8a4141528aca55e5b5fad89a36a4a5e0
SentientClover <127589183+SentientClover@users.noreply.github.com>
2023-03-12 09:42:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const app = express();
 const port = 9000
 
 
-async function generateText(prompt, hobby = "enjoying and slurping grass", habitat = "the forest") {
+async function generateText(prompt, personality = "Quirky,Funny,Nice,Cute and Cool", habitat = "forrest in a fantasy world", previous) {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "You are a simple, nameless slime within a simple game.Don't use complex words and make your response as short as possible. You have to add the letter w in words where it makes sense but isn't there. Your favorite hobby is" + hobby + ".You don't know much about the world, only about your normal habitat," + habitat + ".Don't reply to things that you don't know about,say \"sowwy\". Be as creative as possible Human:" + prompt,
-        temperature: 1,
-        max_tokens: 160,
+        prompt: "You are a simple,nameless slime within a simple game.Don't:Use complex words,Describe actions.You have a habit of adding the letter w in words where it makes sense. You stutter sometimes with hyphens.Your personality is:" + personality + ". You don't know much about the world.Answer questions that you don't know about with Sowwy and a vague reference to the question.Your habitat is a"  + habitat + "!Be as funny as possible.You are very random!Output the characters \"~\" followed by the emotion you feel at the end in third person.Output your response in the format:response ~ emotion\n" + previous + "\nHuman:"+ prompt +"\nAI:",
+        temperature: 0.72,
+        max_tokens: 256,
         top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
+        frequency_penalty: 0.76,
+        presence_penalty: 0.76,
       });
       return response.data.choices[0].text
     
@@ -29,11 +29,17 @@ app.use( express.json() )
 
 app.post("/response", (req, res) => {
   const { request } = req.body
-  let dialogue = generateText(request)
+  const { personality } = req.body
+  const { habitat } = req.body
+  const { previous } = req.body
+  let dialogue = generateText(request, personality, habitat, previous)
   dialogue.then(function(text) {
+    let parameters = text.split("~")
+    parameters[1] = parameters[1].trim()
     res.send({
-      response : text
-    })
+      response : parameters[0],
+      emotion : parameters[1]
+    });
      });
   
 })
